390ad7d6717f9c89ebca2381917f391bd3b3e7c9
PARTH AGRAWAL <94750839+parth-agrawall@users.noreply.github.com>
2023-08-15 00:11:34 +0530

    Update day-6.cpp

--- day-6.cpp
+++ day-6.cpp
@@ -1,3 +1,35 @@
+/*
+Task
+Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note:  is considered to be an even index.
+
+Example
+
+
+Print abc def
+
+Input Format
+
+The first line contains an integer,  (the number of test cases).
+Each line  of the  subsequent lines contain a string, .
+
+Constraints
+
+Output Format
+
+For each String  (where ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.
+
+Sample Input
+
+2
+Hacker
+Rank
+Sample Output
+
+Hce akr
+Rn ak
+*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
